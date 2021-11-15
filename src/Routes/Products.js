772a92c5bd4aa988b0108180dbdf1cce56f609fd
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-16 01:15:42 +0600

    products api routes added

--- src/Routes/Products.js
+++ src/Routes/Products.js
@@ -5,5 +5,32 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json({ msg: "get all products" });
 });
-
+// GET: get product by id
+router.get("/:id", (req, res) => {
+  res.json({ id: req.params.id, name: "Laptop" });
+});
+// POST: store product
+router.post("/", (req, res) => {
+  res.json({
+    id: req.params.id,
+    name: "Laptop",
+    msg: "product inserted successfully!",
+  });
+});
+// PUT: update product by id
+router.put("/:id", (req, res) => {
+  res.json({
+    id: req.params.id,
+    name: "Laptop",
+    msg: "Product update successfully!",
+  });
+});
+// DELETE: delete product by id
+router.delete("/:id", (req, res) => {
+  res.json({
+    id: req.params.id,
+    name: "Laptop",
+    msg: "Product was deleted successfully!",
+  });
+});
 module.exports = router;
