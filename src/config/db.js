f1fc091909aa47d9da4e2761cab055347a89942b
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-17 08:53:06 +0600

    change database name and testing console

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 // DB connection
-const MONGODB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3lrf.mongodb.net/woobajar?retryWrites=true&w=majority`;
+const MONGODB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3lrf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose
   .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     //don't show the log when it is test
-    if (process.env.NODE_ENV !== "test") {
+    if (process.env.NODE_ENV === "test") {
       console.log("Connected to %s", MONGODB_URL);
-      console.log("App is running with mongodb ... \n");
+      console.log("App is running with index.js ... \n");
       console.log("Press CTRL + C to stop the process. \n");
     }
   })
