2415d9550032f604bec4997ed008838c4d28d623
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-17 07:22:52 +0600

    deploy heroku server

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.json({ index: "Restful Api with Node Express Mongodb" });
+  res.json({
+    index: "Restful Api with Node Express Mongodb",
+    server: "welcome heroku",
+  });
 });
 
 module.exports = router;
