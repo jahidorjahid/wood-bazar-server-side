7ba39c37ab3cda89eadf99e0697ec6a4ad73dce3
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-19 11:59:39 +0600

    add two api

--- src/Routes/Api.js
+++ src/Routes/Api.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const productsRouter = require("./Products");
+const ordersRouter = require("./Orders");
+const usersRouter = require("./Users");
 
 // products api routers
 app.use("/products/", productsRouter);
+// orders api routers
+app.use("/orders/", ordersRouter);
+// user api routers
+app.use("/users/", usersRouter);
 
 module.exports = app;
